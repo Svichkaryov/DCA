a442782844f82481c62aab0817369c79b35f53fd
Ivan Svichkaryov <sv.ivan1996@gmail.com>
2018-10-31 18:23:32 +0200

    deleted all cipher version except m_n~32_64

--- DCA/Ciphers/Simon.h
+++ DCA/Ciphers/Simon.h
@@ -1,81 +1,31 @@
 #pragma once
+#include "CipherARX_32_64.h"
 
-#define WORD_SIZE 16
-#define KEY_WORDS 4
-
-#if (WORD_SIZE == 64)
-	#define WORD_MASK (0xffffffffffffffffull)
-#else
-	#define WORD_MASK ((0x1ull << (WORD_SIZE&63)) - 1)
-#endif
-
+#define WORD_MASK ((0x1ull << (16&63)) - 1)
 #define CONST_C ((0xffffffffffffffffull ^ 0x3ull) & WORD_MASK)
+#define ROUNDS (32)
+#define CONST_J (0)
 
-#if (WORD_SIZE == 16)
-	#define ROUNDS (32)
-	#define CONST_J (0)
 
-#elif (WORD_SIZE == 24)
-	#if (KEY_WORDS == 3)
-		#define ROUNDS (36)
-		#define CONST_J (0)
-	#elif (KEY_WORDS == 4)
-		#define ROUNDS (36)
-		#define CONST_J (1)
-	#endif
-
-#elif (WORD_SIZE == 32)
-	#if (KEY_WORDS == 3)
-		#define ROUNDS (42)
-		#define CONST_J (2)
-	#elif (KEY_WORDS == 4)
-		#define ROUNDS (44)
-		#define CONST_J (3)
-	#endif
-
-#elif (WORD_SIZE == 48)
-	#if (KEY_WORDS == 2)
-		#define ROUNDS (52)
-		#define CONST_J (2)
-	#elif (KEY_WORDS == 3)
-		#define ROUNDS (54)
-		#define CONST_J (3)
-	#endif
-
-#elif (WORD_SIZE == 64)
-	#if (KEY_WORDS == 2)
-		#define ROUNDS (68)
-		#define CONST_J (2)
-	#elif (KEY_WORDS == 3)
-		#define ROUNDS (69)
-		#define CONST_J (3)
-	#elif (KEY_WORDS == 4)
-		#define ROUNDS (72)
-		#define CONST_J (4)
-	#endif
-
-#endif
-
-using block_t = uint64_t;
-
-class Simon
+class Simon : public CipherARX_32_64
 {
 public:
-	Simon() = default;
+	Simon() : CipherARX_32_64() {};
+	Simon(OutputStateStategy oss, int nBitOuput) : CipherARX_32_64(oss, nBitOuput) {};
 	~Simon() = default;
 
-	inline block_t S(block_t state, int distance);
-	inline block_t f(block_t state);
+	inline uint16_t S(uint16_t state, int distance);
+	inline uint16_t f(uint16_t state);
 
-	void encrypt_block(const block_t plaintext[2], const block_t key[KEY_WORDS], 
-		block_t ciphertext[2]);
-	void decrypt_block(const block_t ciphertext[2], const block_t key[KEY_WORDS],
-		block_t plaintext[2]);
+	void encrypt_block(const uint16_t plaintext[2], const uint16_t key[4],
+		uint16_t ciphertext[2]);
+	void decrypt_block(const uint16_t ciphertext[2], const uint16_t key[4],
+		uint16_t plaintext[2]);
 	
-	void partial_encrypt_block(const block_t plaintext[2], const block_t key[KEY_WORDS], 
-		block_t ciphertext[2], int rounds, int startInRound = 0);
-	void partial_decrypt_block(const block_t ciphertext[2], const block_t key[KEY_WORDS], 
-		block_t plaintext[2], int rounds, int startInRound = ROUNDS);
+	void partial_encrypt_block(const uint16_t plaintext[2], const uint16_t key[4],
+		uint16_t ciphertext[2], int rounds, int startInRound = 0);
+	void partial_decrypt_block(const uint16_t ciphertext[2], const uint16_t key[4],
+		uint16_t plaintext[2], int rounds, int startInRound = ROUNDS);
 
 private:
 	
--- DCA/Ciphers/Speck.h
+++ DCA/Ciphers/Speck.h
@@ -9,8 +9,6 @@ public:
 	Speck(OutputStateStategy oss, int nBitOuput) : CipherARX_32_64(oss, nBitOuput) {};
 	~Speck() = default;
 
-	// SPECK_32_64
-	
 	inline void round_func(uint16_t& x, uint16_t& y, const uint16_t key);
 	inline void reverse_round_func(uint16_t& x, uint16_t& y, const uint16_t key);
 
@@ -19,15 +17,6 @@ public:
 	void decrypt_block(const uint16_t ciphertext[2], const uint16_t key[4], 
 		uint16_t plaintext[2]) override;
 
-	// SPECK_64_128
-
-	inline void round_func(uint32_t& x, uint32_t& y, const uint32_t key);
-	inline void reverse_round_func(uint32_t& x, uint32_t& y, uint32_t key);
-
-	void encrypt_block(const uint32_t plaintext[2], const uint32_t key[4],
-		uint32_t ciphertext[2]);
-	void decrypt_block(const uint32_t ciphertext[2], const uint32_t key[4],
-		uint32_t plaintext[2]);
 private:
 
 };
